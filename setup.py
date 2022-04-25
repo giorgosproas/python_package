b37fdd89a3c039ff162ce79d0d27f8a12c7c6443
giorgosproas <giorgosproas@gmail.com>
2022-04-25 23:03:31 +0200

    Testing setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_package',
+    version='0.1.0',
+    packages=find_packages(include=['python_package', 'python_package.*'])
+)
