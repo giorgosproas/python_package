9825e972808e50eeca2bf53c951406b2b66be4bf
giorgosproas <giorgosproas@gmail.com>
2022-04-25 23:07:27 +0200

    Testing required libraries.

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='python_package',
     version='0.1.0',
-    packages=find_packages(include=['python_package', 'python_package.*'])
+    packages=find_packages(include=['python_package', 'python_package.*']),
+    install_requires=[
+        'PyYAML',
+        'pandas==0.23.3',
+        'numpy>=1.14.5'
+    ],
 )
