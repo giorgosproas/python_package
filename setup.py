2a64b8ca4a92d517cd458e87ec5c50ea8fe34550
giorgosproas <giorgosproas@gmail.com>
2022-05-16 23:27:34 +0200

    test

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python_package',
+    name='test_package',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(include=['python_package', 'python_package.*','test_package','test_package.*']),
     install_requires=[
         'PyYAML',
         'requests==2.17.0'
