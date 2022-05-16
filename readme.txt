1c088422fbe63f0efd15f6e3980ee2c14a7d2f64
giorgosproas <giorgosproas@gmail.com>
2022-05-16 23:12:12 +0200

    Testing test_package.

---
+++ readme.txt
@@ -0,0 +1,1 @@
+pip install git+https://github.com/giorgosproas/python_package
--- setup.py
+++ setup.py
@@ -9,3 +9,13 @@ setup(
         'requests==2.17.0'
     ],
 )
+
+setup(
+    name='test_package',
+    version='0.1.0',
+    packages=find_packages(include=['test_package', 'test_package.*']),
+    install_requires=[
+        'PyYAML',
+        'requests==2.17.0'
+    ],
+)
