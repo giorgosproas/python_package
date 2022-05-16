eb8159e3d0f96ff04645cd813ed663367df31fa5
giorgosproas <giorgosproas@gmail.com>
2022-05-16 23:54:26 +0200

    test

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='gkutils',
     version='0.1.0',
-    packages=find_packages(include=['gkutils']),
+    packages=find_packages(include=['gkutils', 'gkutils.*']),
     install_requires=[
         'PyYAML',
         'requests==2.17.0'
